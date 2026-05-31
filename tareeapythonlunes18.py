e58b48af278d0ebd1388b02aa0f4b67ed2fe26ab
lucialagosnunez-chile-1988 <luccalucanez@gmail.com>
2026-05-31 13:22:21 -0400

    Add files via upload

---
+++ tareeapythonlunes18.py
@@ -0,0 +1,23 @@
+print("Bienvenido a la Posada del Aventurero!")
+nombre = input("¿Cuál es tu nombre? ")
+
+respuesta = input("¿Qué te vas a servir? tenemos: pescado frito/ lentejas/ pollo asado ")
+
+if respuesta == "pescado frito":
+    print("Aquí tienes tu orden, ¡Buen provecho!")
+
+elif respuesta == "lentejas":
+    print("Aquí tienes tu orden, ¡Buen provecho!")
+
+
+elif respuesta == "pollo asado":
+    print("Aquí tienes tu orden, ¡Buen provecho!")
+
+
+respuesta2 = input("¿Deseas descansar en la posada? si/no ")
+
+if respuesta2 == "si":
+    print("has descansado en la posada del aventurero, vuelve pronto")
+
+elif respuesta2 == "no":
+    print("te has servido una comida en la posada del aventurero, vuelve pronto")
